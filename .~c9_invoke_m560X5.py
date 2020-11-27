0a9912c1f42f851746d1ecbb266f873ccb05c86f
georgenageeb18 <georgenageeb18@gmail.com>
2020-11-27 22:20:41 +0000

    Added Oskar's Stats Page

--- .~c9_invoke_m560X5.py
+++ .~c9_invoke_m560X5.py
@@ -1,16 +1,15 @@
 import os
 import datetime
 import re
-
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session,  url_for, Response
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
-from helpers import apology, login_required, lookup, back_one_day
+from helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
@@ -42,173 +41,6 @@ db = SQL("sqlite:///journal.db")
 
 #A global variable to save the transaction type in, need to display the alert in the index page
 alertMessage = ""
-
-
-@app.route("/")
-@login_required
-def index():
-    user_id = session["user_id"]
-    # activities = [["Exercise", "Eat Healthy", "Drink Water" , "Yoga", "Sleep Early"],
-    # ["Give", "Meditate", "Music" , "Podcast", "Pray", "Relax", "Walk"],["Gaming", "Movies", "Reading" , "Woodworking"],["Date", "Family", "Friends" , "Party", "Video Chat"]
-    # ,["Cleaning", "Coding", "Exam" , "Laundry", "Meeting", "Shopping", "Study"]]
-    # for item in range(0,len(activities)):
-    #     for items in activities[item]:
-    #         db.execute("INSERT into activities(activity_name, category_id) values(?,?);", items,item+1)
-    return render_template("index.html")
-
-
-@app.route("/new_entries", methods=["GET", "POST"])
-@login_required
-def new_entries():
-    time  = datetime.datetime.now().astimezone()
-    if request.method == "POST":
-        user_id = session["user_id"]
-        print("Result")
-        score = request.form["btn-emotion"]
-
-        day = time.strftime("%d")
-        month = time.strftime("%m")
-        year = time.strftime("%Y")
-        hour = time.strftime("%H")
-        minute = time.strftime("%M")
-        db.execute("Insert into user_scores (user_id, score,day,month,year,hour,minute) values(?,?,?,?,?,?,?)",user_id,score,day,month,year,hour,minute)
-        id = db.execute("SELECT entry_id from user_scores where user_id = ? and score = ? and day = ? and month = ? and year = ? and hour = ? and minute = ?;",user_id,score,day,month,year,hour,minute)
-
-        return redirect(url_for('activities',id=id))
-    else:
-        user_id = session["user_id"]
-        entry = request.form.get("btn-emotion")
-        data = [1,2,3,4,5]
-        length = len(data)
-        timeOfEntry = time.strftime("%a, %b %d, %I:%M %p")
-        return render_template("new_entries.html",len=length,data=data,timeOfEntry = timeOfEntry)
-
-#Activties Method
-@app.route("/activities", methods=["GET", "POST"])
-@login_required
-def activities():
-    id = request.args.get("id")
-    print("ID:",id)
-    if request.method == "POST":
-        return redirect("/questions")
-    else:
-        user_id = session["user_id"]
-        preferences = db.execute("SELECT preference_id from preferences where user_id = ?;", user_id)
-        print(preferences)
-        activityImagePaths=[]
-        activityCategoryTitles=[]
-        activityTitles = []
-        activities=[]
-
-        for index in range(0,len(preferences)):
-            activities.append(db.execute("SELECT category_name,activity_name from activities JOIN categories ON activities.category_id = categories.category_id where categories.category_id=?;",preferences[index]["preference_id"]))
-        for outerIndex in range(len(activities)):
-            for item in range(0,len(activities[outerIndex])):
-                print(outerIndex)
-                category = activities[outerIndex][item]['category_name']
-                if category not in activityCategoryTitles:
-                    activityCategoryTitles.append(category)
-                activity = activities[outerIndex][item]['activity_name']
-                activityTitles.append(activity)
-                print(category,activity)
-                tmp = "/static/icons/"+category+"/"+activity+".png"
-                activityImagePaths.append(tmp)
-        print(activityImagePaths)
-        length = len(activityImagePaths)
-        return render_template("activities.html",len=length,activities=activityImagePaths,activityTitles=activityTitles,categoryTitles=activityCategoryTitles,categories=len(activityCategoryTitles))
-
-#Questions Method
-@app.route("/questions", methods=["GET", "POST"])
-@login_required
-def questions():
-    if request.method == "POST":
-        return redirect("/history")
-    else:
-        return render_template("questions.html")
-
-#History Method
-@app.route("/history", methods=["GET", "POST"])
-@login_required
-def history():
-    if request.method == "POST":
-        return redirect("/")
-    else:
-        return render_template("history.html")
-
-@app.route("/stats", methods=["GET", "POST"])
-@login_required
-def stats():
-    """
-    Displays the perosnal statistics for the user
-    """
-    user_id = session["user_id"]
-    time  = datetime.datetime.now().astimezone()
-    day = int(time.strftime("%-d"))
-    month = int(time.strftime("%-m"))
-    year = int(time.strftime("%Y"))
-
-    try:
-        scope = int(request.args.get("range"))
-    except TypeError:
-        scope = 7
-
-    print(scope, type(scope))
-    # Adjusting for difference in days in a month (Getting scores up until the same date a month before)
-    if scope == 30:
-        if month == 3:
-            scope = 28
-        elif (month == 1 or month == 2 or month == 4 or month == 6 or month == 8 or month == 9 or month == 11):
-            scope = 31
-
-    x = []
-    y = []
-
-    # Find the average scores of the last 7 days individually and append them to x, and y
-    for i in range(scope):
-        avg_score = db.execute("SELECT AVG(score) FROM user_scores WHERE user_id = ? AND Month = ? AND Day = ? AND Year = ?", session["user_id"], month, day, year)
-        formatted_date = f"{month}-{day}-{year}"
-        x.append(formatted_date)
-        try:
-            y.append(float(avg_score[0]['AVG(score)']))
-        except TypeError:
-            y.append(0)
-
-        # Moving one day back in time -- move to helpers
-        month, day, year = back_one_day(month, day, year)
-
-    # Reversing the lists to get them in the right order
-    x.reverse()
-    y.reverse()
-    #print(x)
-    #print(y)
-
-    # Setting the title and adjusting labels
-    if scope == 7:
-        title = 'Happiness over the last Week'
-    elif scope > 27 and scope < 32:
-        title = 'Happiness over the last Month'
-    else:
-        title = 'Happiness over the last Year'
-        count = 0
-        # Only keeping every 10th label for displaying purposes
-        for i in range(len(x)):
-            count += 1
-            if count == 10:
-                count = 0
-                continue
-            else:
-                x[i] = ''
-
-    return render_template("stats.html", title=title, max=5, labels=x, values=y)
-
-
-@app.route("/entries", methods=["GET","POST"])
-@login_required
-def entries():
-    if request.method == "POST":
-            return redirect(request.form["page-btn"])
-    else:
-        return render_template("entries.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -261,6 +93,172 @@ def logout():
     return redirect("/")
 
 
+
+@app.route("/new_entries", methods=["GET", "POST"])
+@login_required
+def new_entries():
+    time  = datetime.datetime.now().astimezone()
+    if request.method == "POST":
+        user_id = session["user_id"]
+        print("Result")
+        score = request.form["btn-emotion"]
+
+        day = time.strftime("%d")
+        month = time.strftime("%m")
+        year = time.strftime("%Y")
+        hour = time.strftime("%H")
+        minute = time.strftime("%M")
+        db.execute("Insert into user_scores (user_id, score,day,month,year,hour,minute) values(?,?,?,?,?,?,?)",user_id,score,day,month,year,hour,minute)
+        #  if request.form["btn-emotion"] == 1:
+        #     print("Terrible")
+        #  print(entry)
+        #  data = {1,2,3,4,5}
+        #  length = "FIVE"
+
+        return redirect("/activities")
+    else:
+        user_id = session["user_id"]
+        entry = request.form.get("btn-emotion")
+        data = [1,2,3,4,5]
+        length = len(data)
+        timeOfEntry = time.strftime("%a, %b %d, %I:%M %p")
+        return render_template("new_entries.html",len=length,data=data,timeOfEntry = timeOfEntry)
+
+#Activties Method
+@app.route("/activities", methods=["GET", "POST"])
+@login_required
+def activities():
+    if request.method == "POST":
+        return redirect("/questions")
+    else:
+        return render_template("activities.html")
+
+#Questions Method
+@app.route("/questions", methods=["GET", "POST"])
+@login_required
+def questions():
+    if request.method == "POST":
+        return redirect("/history")
+    else:
+        return render_template("questions.html")
+
+#History Method
+@app.route("/history", methods=["GET", "POST"])
+@login_required
+def history():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("history.html")
+
+@app.route("/stats", methods=["GET", "POST"])
+@login_required
+def stats():
+    user_id = session["user_id"]
+    # rows = db.execute("SELECT score,day,month,year FROM user_scores where user_id=? and day=23;",user_id)
+    days = 0
+    year = 2020
+    points = [{}]
+    time  = datetime.datetime.now().astimezone()
+    currentyear = time.strftime("%Y")
+    for year in range(year, int(currentyear)+1):
+        for month in range(1,12):
+            # Check the month
+            if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+                days = 31
+            elif month == 2 and year % 4 ==0 :
+                days = 29
+            elif month == 2:
+                days = 28
+            else:
+                days = 30
+            for day in range(1, days):
+                average = db.execute("Select AVG(score) from user_scores where user_id=? and day=? and month=? and year=?" , user_id, day, month, year)
+                if not average[0]['AVG(score)'] is None:
+                    dict = {}
+                    dict['avg'] = average
+                    dict['day'] = day
+                    points.append(dict)
+                else:
+                    continue
+    print(points)
+
+
+
+
+    return render_template("stats.html")
+
+@app.route("/entries", methods=["GET","POST"])
+@login_required
+def entries():
+    if request.method == "POST":
+            return redirect(request.form["page-btn"])
+    else:
+        return render_template("entries.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("must provide email", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for email
+        rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
+
+        # Ensure email exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid email and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "POST":
+        result = lookup(request.form.get("symbol"))
+        if result is None:
+            return apology("Invalid Symbol", 400)
+        else:
+            companyName = result["name"]
+            symbolText = result["symbol"]
+            price = usd(result["price"])
+            return render_template("quote.html",name=companyName,symbol=symbolText,stockPrice=price,notSearched=False)
+    else:
+        return render_template("quote.html",notSearched=True)
+
 @app.route("/change-password", methods=["GET", "POST"])
 def changePassword():
     if request.method == "POST":
@@ -302,7 +300,7 @@ def register():
         if not email:
             return apology("must provide email", 400)
 
-         # Query dataxbase for email
+         # Query database for email
         rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
 
         #  Ensure email is unique
@@ -329,7 +327,7 @@ def register():
         # Register User into the database
 
         passwordHash = generate_password_hash(request.form.get("password"))
-        db.execute("INSERT INTO users (email, hash, firstTime) VALUES(?, ?, ?);", email, passwordHash, 1)
+        db.execute("INSERT INTO users (email, hash) VALUES(?, ?)", email, passwordHash)
 
         # Redirect user to login page
         return redirect("/login")
@@ -339,21 +337,52 @@ def register():
         return render_template("register.html")
 
 
-@app.route("/preferences", methods=["GET", "POST"])
+@app.route("/sell", methods=["GET", "POST"])
 @login_required
-def preferences():
-    #THIS LINE DOESNT WORK
+def sell():
+    # Get current timestomp, Reference: https://timestamp.online/article/how-to-convert-timestamp-to-datetime-in-python
+    time  = datetime.datetime.now().astimezone()
+    formattedTS = time.strftime("%Y-%m-%d %H:%M:%S")
+    """Sell shares of stock"""
+    symbol = request.form.get("symbol")
     if request.method == "POST":
-        preferences = request.form.getlist('cb')
-        for preference in preferences:
-            p_id = preference.replace('/','')
-            integerId = int(p_id)
-            userID = session['user_id']
-            db.execute("Insert into preferences (user_id, preference_id) values(?,?);",userID,integerId)
-        return render_template("index.html")
+        user_id = session["user_id"]
+        result = lookup(symbol)
+        shares = request.form.get("shares")
+        if not symbol:
+            return apology("Missing Symbol",400)
+        elif not shares:
+            return apology("Missing Shares",400)
+        elif int(shares) <= 0:
+            return apology("Shares Must Be Positive",400)
+        else:
+            numberOfSharesAvailable = db.execute("Select shares from user_stocks where user_id=? and stock_symbol=?;",session["user_id"],symbol)
+            currentShares = numberOfSharesAvailable[0].get("shares")
+            newShares = int(currentShares) - int(shares)
+            currentUserCashDict = db.execute("SELECT cash from users where id=?",user_id)
+            userCash = currentUserCashDict[0].get("cash")
+            stockPrice = result["price"]
+            totalPrice = float(shares) * float(stockPrice)
+            sum = float(userCash) + totalPrice
+            if int(shares) > currentShares:
+                return apology("Too Many Shares",400)
+            if int(newShares) == 0:
+                db.execute("DELETE FROM user_stocks WHERE user_id=? AND stock_symbol = ?", user_id,symbol)
+            else:
+                db.execute("UPDATE user_stocks SET shares = ? WHERE user_id = ? AND stock_symbol = ?", newShares, user_id,symbol)
+        db.execute("UPDATE users SET cash = ? WHERE id = ?",sum,user_id)
+        db.execute("INSERT INTO transactions (transaction_id, symbol, shares, price, timestamp) VALUES(?,?,?,?,?)",user_id, symbol, - int(shares), totalPrice, formattedTS)
+        global alertMessage
+        alertMessage = "Sold!"
+        return redirect("/")
 
     else:
-        return render_template("preferences.html")
+        symbols = db.execute("Select stock_symbol from user_stocks where user_id=?",session["user_id"])
+        symbolsList = []
+        for symbol in symbols:
+            symbolsList.append(symbol['stock_symbol'])
+        return render_template("sell.html",len=len(symbolsList),symbols=symbolsList)
+
 
 
 
